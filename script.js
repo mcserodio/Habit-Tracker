4c6112ad74fcfcbf409bc94a7a5a10fa302b2cc5
newyorkla99 <140859033+newyorkla99@users.noreply.github.com>
2023-12-13 10:34:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function displayText() {
+  let userinput = document.getElementById("strpoints").value;
+  document.getElementById("str-stat").textContent = userinput;
+}
+function DEFfunction() {
+  let userinput = document.getElementById("defpoints").value;
+  document.getElementById("def-stat").textContent = userinput;
+}
+function WSDfunction() {
+  let userinput = document.getElementById("wsdpoints").value;
+  document.getElementById("wsd-stat").textContent = userinput;
+}
+function SPDfunction() {
+  let userinput = document.getElementById("spdpoints").value;
+  document.getElementById("spd-stat").textContent = userinput;
+}
+function LVLfunction() {
+  let userinput = document.getElementById("level").value;
+  document.getElementById("player-level").textContent = userinput;
+}
+function habitFunction() {
+  let userinput = document.getElementById("habit").value;
+  document.getElementById("habit-tracked").textContent = userinput;
+}
